77a393a715f16c200284f3ab560b3f45ceaadcd8
atvo912 <atvo912@gmail.com>
2021-04-14 17:22:26 -0700

    "-Updated state of App.jsx -Created config.js to hold API key in a .gitignore file -Created style.css and linked to index.html"

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import API_KEY from './config.js'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      products: [],
+      currentProduct: {}
+    };
+  }
+
+  componentDidMount() {
+    axios({
+      method: 'get',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products',
+      headers: {'Authorization': API_KEY}
+    })
+    .then(({data: products} = res) => {
+      this.setState({products: products, currentProduct: products[0]});
+    })
+    .catch((err) => {
+      console.error(err);
+    })
   }
 
   render() {
