769a4a430e43f26e9c87e5d11f271c8f20466d24
kwakster4 <kimbyung4@utexas.edu>
2021-04-16 13:53:30 -0500

    working on ticket 2.1 tried to find total count of reviews through metadata, but metadata shows count of all reviews, including reported, whereas views only shows non-reported reviews. Gotta figure out how to reconcile the two.

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -11,18 +11,25 @@ class Reviews extends React.Component {
     this.state = {
       reviews: [],
       meta: {},
-      sortType: 'relevant'
+      sortType: 'relevant',
+      count: 2
     };
     // sortTypes are either newest, helpful, or relevant
   }
 
-  componentDidUpdate(prevProps) {
+  // on more reviews button click, count goes up by two.
+  // overall reviews count = meta.recommended.false + .true;
+  // if meta.recommended.false + meta.recommended.true < count, remove the count button.
+  // updates component if state changes. Maybe check for a specific state change to prevent infinite loop of changing due to state.
+  // OR
+  // can handle axios request on the button click rather than only on the componentDidUpdate!
+
+  componentDidUpdate(prevProps, prevState) {
     // Component updates when changes to prop or state.
     // can set up so both axios request go asynchronously, or set it up so it goes one at a time.
     // prefer to have it so that this.setState works at the same time.
     // Promise.all.
     // updates component if the props changes.
-    // What if state changes? More specifically, the state for sortType?
     if (this.props.product !== prevProps.product) {
       let promises = [];
       promises.push(axios({
@@ -56,12 +63,20 @@ class Reviews extends React.Component {
   }
 
   render() {
+    // entire premise of finding from totalCount is off.
+    // maybe try to find all reviews at the beginning?
+    let totalCount = 0;
+    if (this.state.meta.recommended) {
+      let recommended = this.state.meta.recommended
+      totalCount = parseInt(recommended.false) + parseInt(recommended.true);
+    }
     return (
       <div id='reviews' className='grid-container reviews'>
         REVIEWS WRAPPER
         <ReviewBreakdown />
         <ReviewSort />
         <ReviewList reviews = {this.state.reviews}/>
+        {(count >= totalCount) }
         <div id='reviews-add'> Add Review </div>
       </div>
     );
