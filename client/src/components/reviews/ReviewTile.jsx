b521de8ca0e34d4dd56bff4caff08da63caf8632
kwakster4 <kimbyung4@utexas.edu>
2021-04-19 15:11:57 -0500

    Working on ticket 2.2 Used moment.js to format correct date Made Summary section into h4 tag, bolded changed stylings to give border to each tile

--- client/src/components/reviews/ReviewTile.jsx
+++ client/src/components/reviews/ReviewTile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 class ReviewTile extends React.Component {
   constructor(props) {
@@ -7,22 +8,37 @@ class ReviewTile extends React.Component {
 
   render() {
     let review = this.props.review;
+    console.log(review.photos)
     return (
       <div id='reviews-tile' className='reviews-component'>
         <div className='reviews star-rating'>
           Stars: {review.rating} stars
         </div>
         <div className='review-date'>
-          Date: {review.date}
+          Date: {moment(review.date).format('MMMM DD, YYYY')}
         </div>
         <div className='review-summary'>
-          Summary: {review.summary}
+          <h4>
+            <b>Summary: {review.summary}</b>
+          </h4>
         </div>
         <div className='review-body'>
           Body: {review.body}
           {/* Images: {review.photos} */}
           {/* What if no images */}
         </div>
+        {(review.photos)
+          ? <div className='review-images'>
+              Images:
+              {review.photos.map((image)=> {
+                return (
+                <div key={image.id}>
+                  {image.url}
+                </div>
+                );
+              })}
+            </div>
+          : null}
         {(review.recommend)
         ? 'I recommend this'
         : null}
