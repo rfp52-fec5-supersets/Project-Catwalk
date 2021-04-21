69b082db67d4a784910a034aa4ffaee79d3649c3
kwakster4 <kimbyung4@utexas.edu>
2021-04-21 17:57:11 -0500

    ticket 2.6.2 recommended radio buttons implemented recommended radio buttons, controlled components

--- client/src/components/reviews/AddReviewForm.jsx
+++ client/src/components/reviews/AddReviewForm.jsx
@@ -8,6 +8,7 @@ class AddReviewForm extends React.Component {
       starRating: 0
     }
     this.handleStar = this.handleStar.bind(this);
+    this.handleRecommended = this.handleRecommended.bind(this);
   }
 
   handleStar(starCount) {
@@ -18,14 +19,25 @@ class AddReviewForm extends React.Component {
     })
   }
 
+  handleRecommended(e) {
+    this.setState({
+      recommended: e.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Write Your Review</h2>
         <h3>About the {this.props.product.name}</h3>
-        <p>Overall Star Rating Here</p>
         <AddStarRating starCount={this.state.starRating} handleClick = {this.handleStar}></AddStarRating>
         <p>Do you recommend product?</p>
+        <div className='add-review recommended-form'>
+          <input type='radio' name='recommend' value={true} id='add-review-recommended-yes' required='required' checked={this.state.recommended === 'true'} onChange={this.handleRecommended}></input>
+          <label htmlFor='add-review-recommended-yes'>Yes</label>
+          <input type='radio' name='recommend' value={false} id='add-review-recommended-no' required='required' checked={this.state.recommended === 'false'} onChange={this.handleRecommended}></input>
+          <label htmlFor='add-review-recommended-no'>No</label>
+        </div>
         <p>Characteristic Section</p>
         <p>Review Summary</p>
         <p>Review Body</p>
