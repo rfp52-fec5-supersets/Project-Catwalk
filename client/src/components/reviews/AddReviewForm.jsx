4397c1ccd41737d108c36f4afa1c5bf294e75552
kwakster4 <kimbyung4@utexas.edu>
2021-04-21 17:17:19 -0500

    ticket 2.6.1 Star Rating implemented star rating starts off at 0 does not update until clicked, i.e. does not do anything on hover fills all stars with color up to the clicked star

--- client/src/components/reviews/AddReviewForm.jsx
+++ client/src/components/reviews/AddReviewForm.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import AddStarRating from './AddStarRating.jsx'
 
 class AddReviewForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      starRating: 0
+    }
+    this.handleStar = this.handleStar.bind(this);
+  }
+
+  handleStar(starCount) {
+    // When AddStarRating Component receives a click, it returns the star Count of the star.
+    // 1-5
+    this.setState({
+      starRating: starCount
+    })
   }
 
   render() {
@@ -11,6 +24,7 @@ class AddReviewForm extends React.Component {
         <h2>Write Your Review</h2>
         <h3>About the {this.props.product.name}</h3>
         <p>Overall Star Rating Here</p>
+        <AddStarRating starCount={this.state.starRating} handleClick = {this.handleStar}></AddStarRating>
         <p>Do you recommend product?</p>
         <p>Characteristic Section</p>
         <p>Review Summary</p>
