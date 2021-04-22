706f15b69e1d52ccd7160ce61db5e008507b79af
kwakster4 <kimbyung4@utexas.edu>
2021-04-22 12:15:01 -0500

    fixed issue with starsDisplay when starFragment is rounded to 0, but no extra empty star

---
+++ client/src/components/reviews/AddReviewForm 2.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class AddReviewForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Add Review Here!
+      </div>
+    );
+  }
+}
+export default AddReviewForm;
