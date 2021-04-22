63f4b00964250dc9762264b9bf8ab1fde04d65f7
atvo912 <atvo912@gmail.com>
2021-04-22 15:32:08 -0700

    Implemented share buttons for Facebook, Twitter, and Pinterest

--- client/src/components/ProductInfo.jsx
+++ client/src/components/ProductInfo.jsx
@@ -17,7 +17,7 @@ class ProductInfo extends React.Component {
 
     return (
       <div className = "product-info">
-        <h1 id = "title"> {this.props.currentProduct.name}</h1>
+        <header id = "title"> {this.props.currentProduct.name}</header>
         <div id = "price">
         {this.props.currentStyle.sale_price ? <div className = "original-price-strikethrough">{'$'+this.props.currentStyle.original_price}</div> : <div className = "original-price">{'$'+this.props.currentStyle.original_price}</div>} {this.props.currentStyle.sale_price && <div className = "sale-price">{'$'+this.props.currentStyle.sale_price}</div>}
         </div>
@@ -39,7 +39,12 @@ class ProductInfo extends React.Component {
         {/* {JSON.stringify(this.props.currentProduct.features)} */}
 
         <div id = "socials">
-        <b>Share:</b> FACEBOOK     TWITTER     PINTEREST
+        <b id = "share-title">Share:</b>
+        <iframe src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2F127.0.0.1%3A8080%2F&layout=button&size=small&width=67&height=20&appId" width="67" height="20" scrolling="no" frameBorder="0" allowFullScreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+
+        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-show-count="false"><span className="label" id="l">Tweet</span></a>
+
+        <a data-pin-do="buttonBookmark" href="https://www.pinterest.com/pin/create/button/"></a>
         </div>
       </div>
     )
