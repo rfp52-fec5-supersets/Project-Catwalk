c697ded56c8a4208c7dc88d4166e1fceeb01b063
Alex <alex.garay94@gmail.com>
2021-04-22 12:17:30 -0700

    Refactored to not use a dependency for modals

--- client/src/components/questions/QuestionAnswerAdd.jsx
+++ client/src/components/questions/QuestionAnswerAdd.jsx
@@ -1,56 +1,58 @@
 import React from 'react';
 import Modal from 'react-modal';
+import QuestionAnswerAddModal from './QuestionAnswerAddModal.jsx';
 
 class QuestionAnswerAdd extends React.Component {
   constructor (props) {
     super(props);
+    this.handleToggle = this.handleToggle.bind(this);
     this.state = {
       showModal: false
-    };
-
-    this.handleOpenModal = this.handleOpenModal.bind(this);
-    this.handleCloseModal = this.handleCloseModal.bind(this);
-  }
-
-  handleOpenModal () {
-    this.setState({ showModal: true });
-  }
-
-  handleCloseModal () {
-    this.setState({ showModal: false });
-  }
-  componentDidMount() {
-    Modal.setAppElement('body');
- }
-  render () {
-    const customStyles = {
-      content : {
-        top                   : '50%',
-        left                  : '50%',
-        right                 : 'auto',
-        bottom                : 'auto',
-        marginRight           : '-50%',
-        transform             : 'translate(-50%, -50%)'
-      }
     }
-    return (
+  }
+
+  handleToggle() {
+    let newState = !this.state.showModal;
+    this.setState({
+      showModal: newState
+    });
+  }
+
+//     this.handleOpenModal = this.handleOpenModal.bind(this);
+//     this.handleCloseModal = this.handleCloseModal.bind(this);
+//   }
+
+//   handleOpenModal () {
+//     this.setState({ showModal: true });
+//   }
+
+//   handleCloseModal () {
+//     this.setState({ showModal: false });
+//   }
+//   componentDidMount() {
+//     Modal.setAppElement('body');
+//  }
+//   render () {
+//     const customStyles = {
+//       content : {
+//         top                   : '50%',
+//         left                  : '50%',
+//         right                 : 'auto',
+//         bottom                : 'auto',
+//         marginRight           : '-50%',
+//         transform             : 'translate(-50%, -50%)'
+//       }
+//     }
+    render() {
+      return (
       <div>
-        <button onClick={this.handleOpenModal}>Add Answer</button>
-        <Modal style={customStyles} ariaHideApp={true} isOpen={this.state.showModal} contentLabel="Minimal Modal Example">
-          <button onClick={this.handleCloseModal}>Close Modal</button>
-          <h1 style={{textAlign: 'center'}}>Add an Answer</h1>
-          <form onSubmit={(event) => event.preventDefault()}>
-            <textarea style={{minHeight: '10em', minWidth: '35em'}} required="required" maxLength="1000" placeholder="Answer"></textarea><br/>
-            <label htmlFor="lname">Nickname:</label><br/>
-            <input placeholder="Example: jack543!" maxLength="60" type="text" id="fname" name="fname"/><br/>
-            <div>For privacy reasons, do not use your full name or email address</div><br/>
-            <label htmlFor="lname">Email:</label><br/>
-            <input placeholder="Example: jack@email.com" maxLength="60" type="text" id="lname" name="lname"/><br/>
-            <div>For authentication reasons, you will not be emailed</div><br/>
-            <input onChange={(event)=> console.log(URL.createObjectURL(event.target.files[0]))} type="file" id="img" name="img" accept="image/*" multiple/><br/>
-            <button>Submit</button>
-          </form>
-        </Modal>
+        <button onClick={this.handleToggle}>Add Answer</button>
+       {this.state.showModal &&
+        <QuestionAnswerAddModal onCloseRequest={this.handleToggle}>
+          <div>
+            Add a Question
+          </div>
+        </QuestionAnswerAddModal>}
       </div>
     );
   }
---
+++ client/src/components/questions/QuestionAnswerAddModal.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class QuestionAnswerAddModal extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {
+      onCloseRequest,
+      children
+    } = this.props;
+    return (
+      <div className='modal-overlay'>
+        <button type="button" className='close-button' onClick={onCloseRequest}>
+          X
+        </button>
+        <div className='modal-content'>
+          {children}
+        </div>
+      </div>
+    );
+  }
+}
+export default QuestionAnswerAddModal;
