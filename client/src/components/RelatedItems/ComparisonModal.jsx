60415903c90c2cf6a239d5a88b97187f8ba7168e
Roy Xue <roythegoose@gmail.com>
2021-04-22 21:41:50 -0700

    CORRECTLY RENDERED COMPARISON INFORMATION IN MODAL WINDOW

--- client/src/components/RelatedItems/ComparisonModal.jsx
+++ client/src/components/RelatedItems/ComparisonModal.jsx
@@ -9,18 +9,20 @@ class ComparisonModal extends React.Component {
 
     let currentFeatures = this.props.currentFeatures;
     let relatedFeatures = this.props.relatedProduct.features;
+    let currentProduct = this.props.currentProduct;
+    let relatedProduct = this.props.relatedProduct;
 
 
     //CURRENT PRODUCT'S FEATURE-VALUE OBJS
-    let currentValues = [];
+    let currentValues = {};
     for (var i = 0; i < currentFeatures.length; i++) {
-      currentValues.push(currentFeatures[i]);
+      currentValues[currentFeatures[i].feature] = currentFeatures[i].value;
     }
 
     //RELATED (COMPARISON) PRODUCT'S FEATURE-VALUE OBJS
-    let relatedValues = [];
+    let relatedValues = {};
     for (var i = 0; i < relatedFeatures.length; i++) {
-      relatedValues.push(relatedFeatures[i]);
+      relatedValues[relatedFeatures[i].feature] = relatedFeatures[i].value;
     }
 
     let features = new Set;
@@ -28,23 +30,47 @@ class ComparisonModal extends React.Component {
     for (var i = 0; i < allFeatures.length; i++) {
       features.add(allFeatures[i].feature)
     }
+    let featuresArray = Array.from(features);
 
-    // console.log('currentProductValues', currentValues);
-    // console.log('relatedProductValues', relatedValues)
-    // console.log('features', features)
+    console.log('currentValues', currentValues);
+    console.log('currentValues', relatedValues)
+    console.log('featuresArray', featuresArray)
 
     return (
       <div className='modal-overlay'>
-        <button type='button' className='close-button' onClick={this.props.onClose}>
-          X
-        </button>
+
+        <button type='button' className='close-button' onClick={this.props.onClose}> X</button>
+
         <div className='comparison-table'>
           COMPARING
-          <div>{this.props.currentProduct.name}</div>
-          <div>{this.props.relatedProduct.name}</div>
+          <div className="left-column">
+            <h1>{currentProduct.name}</h1>
+            <div>{featuresArray.map(feature => {
+              console.log('current value feature', currentValues);
+              if (currentValues[feature] === undefined) {
+                return <div>{<br />}</div>
+              } else {
+                return <div>{currentValues[feature]}</div>
+              }
+            })}</div>
+          </div>
 
+          <div className="middle-column">{featuresArray.map(feature => { return <div>{feature}</div> })}</div>
+
+          <div className="right-column">
+            <h1>{relatedProduct.name}</h1>
+            <div>{featuresArray.map(feature => {
+              if (relatedValues[feature] === undefined) {
+                return <div>{<br />}</div>
+              } else {
+                return <div>{relatedValues[feature]}</div>
+              }
+            })}</div>
+          </div>
         </div>
+
       </div>
+
     )
   }
 }
