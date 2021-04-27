a0e01b23ca0ed0ae1d33ca4550714e24210ecb9f
kwakster4 <kimbyung4@utexas.edu>
2021-04-26 21:16:18 -0500

    tests for reviews, forgot to stage these files with last commit look at previous commit for comments

---
+++ client/src/dummyProps.js
@@ -0,0 +1,54 @@
+const ReviewProps = {
+  "product": {
+      "id": 17071,
+      "campus": "hr-rfp",
+      "name": "Heir Force Ones",
+      "slogan": "A sneaker dynasty",
+      "description": "Now where da boxes where I keep mine? You should peep mine, maybe once or twice but never three times. I'm just a sneaker pro, I love Pumas and shell toes, but can't nothin compare to a fresh crispy white pearl",
+      "category": "Kicks",
+      "default_price": "99.00",
+      "created_at": "2021-02-23T04:22:44.728Z",
+      "updated_at": "2021-02-23T04:22:44.728Z"
+  },
+  "reviewMeta": {
+      "product_id": "17071",
+      "ratings": {
+          "1": "1",
+          "2": "1",
+          "3": "5",
+          "4": "2",
+          "5": "3"
+      },
+      "recommended": {
+          "false": "5",
+          "true": "7"
+      },
+      "characteristics": {
+          "Size": {
+              "id": 57235,
+              "value": "3.2222222222222222"
+          },
+          "Width": {
+              "id": 57236,
+              "value": "3.4444444444444444"
+          },
+          "Comfort": {
+              "id": 57237,
+              "value": "3.3333333333333333"
+          },
+          "Quality": {
+              "id": 57238,
+              "value": "3.4444444444444444"
+          }
+      }
+  },
+  "averageRating": "3.42",
+  "ratings": {
+      "1": "1",
+      "2": "1",
+      "3": "5",
+      "4": "2",
+      "5": "3"
+  }
+};
+export {ReviewProps};
