b4e7fa198ac702428ab2b7c0b87ab4fbd82598ba
kwakster4 <kimbyung4@utexas.edu>
2021-04-27 16:40:30 -0500

    placed server.js in right folder

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-app.use(express.static(path.join(__dirname,'client/dist/')));
+app.use(express.static(path.join(__dirname,'../client/dist/')));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
